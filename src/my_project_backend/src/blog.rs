43b66c78a833f0e43ccd0d68e1d5acbe2c813349
Immy Shack <Sdfmario46@gmail.com>
2024-12-03 19:10:05 +0000

    blog system

--- src/my_project_backend/src/blog.rs
+++ src/my_project_backend/src/blog.rs
@@ -1,5 +1,21 @@
-struct Blog {
+use candid::CandidType;
+use ic_cdk::api::time;
+
+#[derive(Clone, CandidType)]
+pub struct Blog {
     title: String,
-    data: u32
+    // date: u64,
+    content: String,
+    tags: Vec<String>
 }
 //unsigned: u8 0-254 2^8 liczba nieujemna która jest potegą 2
+impl Blog {
+     pub fn new(title: String, /*date: u64,*/ content: String, tags: Vec<String>) -> Self{
+        Self {
+            title,
+            //date: time(),
+            content,
+            tags
+        }
+    }
+}
--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,20 +1,38 @@
 use std::cell::RefCell;
-
+use crate::blog::Blog;
 mod blog;
 thread_local!{
-    static BLOGS: RefCell<Vec<String>> = RefCell::new(Vec::new());
+    static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
+//walidacja: content <= 500, vec tagi <= 3
 //Title
 //Data
 //tagi
-//Komentare ?
+//Komentarze ?
 #[ic_cdk::update]
-fn add_blog(new_blog: String){
-    BLOGS.with(|blogs| blogs.borrow_mut().push(new_blog));
+fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String>{
+   //VALIDATION
+    if title.len() >= 250{
+        return Err("Title is too long!".to_string())
+    }
+    if content.len() >= 500 {
+        return Err("Content is too long!".to_string())
+    }
+    if tags.len() >= 3 {
+        return Err("Too many tags!".to_string())
+    }
+    let blog = Blog::new(title, content, tags);
+    BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
+    let last_blog = BLOGS.with(|blogs|
+        blogs
+        .borrow()
+        .last()
+        .expect("Vec should not be empty").clone());
+    Ok(last_blog)
 }
 
 #[ic_cdk::query]
-fn get_blogs() -> Vec<String> {
+fn get_blogs() -> Vec<Blog> {
     BLOGS.with(|blogs| blogs.borrow().clone())
 }
 
