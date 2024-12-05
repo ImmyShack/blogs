ea1ec573c9085e1e539425fd8d09866a82d223ce
Immy Shack <Sdfmario46@gmail.com>
2024-12-05 22:01:41 +0000

    tailwindcss

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -19,8 +19,7 @@ fn add_config(new_config: Config) {
 fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String>{
     ic_cdk::println!("Trying to add blog: (title: {}, content: {}, tags: {:?})", title, content, tags);
     let config = CONFIG.with(|config| config.borrow().clone());
-    //VALIDATION
-    if title.len() > config.max_title_len as usize{
+    if title.len() > config.max_title_len as usize {
         return Err("Title is too long!".to_string())
     }
     if content.len() > config.max_content_len as usize {
@@ -31,22 +30,28 @@ fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, S
     }
     let are_tags_in_config_tags = tags.iter().any(|tag| !config.tags.contains(tag));
     if are_tags_in_config_tags {
-        return Err("Tags not in config tags!".to_string())
+        return Err("Tags are not valid!".to_string()) 
     }
+    
     let blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
-    let last_blog = BLOGS.with(|blogs|
+    let last_blog = BLOGS.with(|blogs| 
         blogs
         .borrow()
         .last()
         .expect("Vec should not be empty").clone());
     Ok(last_blog)
 }
-
 #[ic_cdk::query]
 fn get_blogs() -> Vec<Blog> {
     BLOGS.with(|blogs| blogs.borrow().clone())
 }
+
+#[ic_cdk::query]
+fn get_config() -> Config {
+    CONFIG.with(|config| config.borrow().clone())
+}
+
 
 #[ic_cdk::query]
 fn greet(name: String) -> String {
